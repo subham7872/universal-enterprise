5ee20e8ab09579923a884bc21c4401ed4585673d
subham7872 <pintuduttafkt@gmail.com>
2026-08-30 22:55:52 +0530

    Remove build artifacts

--- frontend/.next/static/chunks/app/brands/nsk/page-e0b7244700d5fb3b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[874,502,48,359],{45805:function(n,e,u){Promise.resolve().then(u.t.bind(u,72972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=45805)}),_N_E=n.O()}]);
